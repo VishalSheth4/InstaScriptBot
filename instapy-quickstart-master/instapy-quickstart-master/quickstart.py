824a7fd8a83d0e2226e889354656e78f93e38727
VishalSheth4 <vishalsheth4@gmail.com>
2020-04-29 16:53:50 +0530

    Update quickstart.py

--- instapy-quickstart-master/instapy-quickstart-master/quickstart.py
+++ instapy-quickstart-master/instapy-quickstart-master/quickstart.py
@@ -19,8 +19,8 @@ comments = ['Nice shot! @{}',
 
 # get an InstaPy session!
 # set headless_browser=True to run InstaPy in the background
-session = InstaPy(username=insta_username,
-                  password=insta_password,
+session = InstaPy(username=life_explorer_4,
+                  password=tigerF1sh00?,
                   headless_browser=False)
 
 with smart_run(session):
